7a0011470c60ab8a664a73b8f38b3608cc37ddc0
Swati Sucharita Samantaray <111977995+swaisamantaray@users.noreply.github.com>
2023-10-11 11:08:18 +0530

    Added files via uploading

---
+++ constants.py
@@ -0,0 +1,9 @@
+consumer_key= 'YXidAFVOykmE54S2YMPqpkdBU'
+consumer_secret= 'BYi2vKGQJfkONoGeoaFrkCWhI8mtheFuxKjCUrlqvc9sRvY7gz'
+
+num_of_tweets = int(30)
+
+access_token = '1549630685257773056-1YHPqpWF5f6LLHYc6YMRT6vjxG9a0P'
+access_token_secret = '7UxzIU8W7nli0dxp2ZHbTir21XyyV9b6LTUkjhk3YS3Sr'
+
+ALPHA_API = 'EAHLZ3GHGD1N6EGF'
